9a5fd2c94e737d2a059ce0904be905a03911046f
JongHwan <42468874+whdghks9241@users.noreply.github.com>
2023-11-23 15:12:21 +0900

    save

--- join/src/main/java/UserInfoServlet.java
+++ join/src/main/java/UserInfoServlet.java
@@ -12,17 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
-
-
-
-
-
-
-
-
-
 @WebServlet("/UserInfoServlet")
 public class UserInfoServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
